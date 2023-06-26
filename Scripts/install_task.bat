2187caae728eec68296b245a6bb0c0f10ddd834e
bellinight <belini.leonardo@gmail.com>
2023-06-26 10:44:38 -0300

    Initial commit

---
+++ Scripts/install_task.bat
@@ -0,0 +1,13 @@
+﻿@echo off
+:: CRIA TAREFA PARA VERIFICAÇÃO DO TOOLKIT
+:: AGENDA MENSAL COM PRIVLILEGIOS ALTOS
+setlocal
+
+set datetimef=%date:~-4%_%date:~3,2%_%date:~0,2%__%time:~0,2%_%time:~3,2%_%time:~6,2%
+
+echo.[INFO] %DATE% %TIME% Deletando Agenda do ProcessaGuard >> "%appdata%\Processa\ToolsManager\log\tkcheck.log"
+schtasks /delete /tn ProcessaGuard /f >> "%appdata%\Processa\ToolsManager\log\tkcheck.log"
+echo.[INFO] %DATE% %TIME% Criando Agendamento do ProcessaGuard >> "%appdata%\Processa\ToolsManager\log\tkcheck.log"
+schtasks /create /tn ProcessaGuard /tr %appdata%\Processa\ToolsManager\ver_rem_toolkit.bat /sc monthly /f /rl HIGHEST >> "%appdata%\Processa\ToolsManager\log\tkcheck.log"
+
+exit
